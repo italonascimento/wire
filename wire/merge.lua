3bc0f1c7e1ed6a5ae8cf337c711dc4d973bec706
Italo Nascimento <italo.abnascimento@gmail.com>
2017-09-26 03:28:38 -0300

    api change: rename wire.assign to wire.merge and make it accept unlimited tables as arguments

---
+++ wire/merge.lua
@@ -0,0 +1,19 @@
+return function (t, ...)
+    local target = {}
+    if type(t) == "table" then
+      local meta = getmetatable(t)
+      for k, v in pairs(t) do target[k] = v end
+      setmetatable(target, meta)
+    else
+      error('Argument must be of type <table>, got <' .. type(t) .. '> instead')
+    end
+
+    for i, tab in ipairs{...} do
+      if type(tab) == 'table' then
+        for k, v in pairs(tab) do target[k] = v end
+      else
+        error('Arguments must be of type <table>, got <' .. type(t) .. '> instead')
+      end
+    end
+    return target
+end
