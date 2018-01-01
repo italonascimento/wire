5eed20977d8571aeffb7a9f9b40aec8372bf0797
Italo Nascimento <italo.abnascimento@gmail.com>
2018-01-01 03:03:11 -0200

    fix error message in merge

--- wire/merge.lua
+++ wire/merge.lua
@@ -1,6 +1,6 @@
 return function (t, ...)
     local target = {}
-    if type(t) == "table" then
+    if type(t) == 'table' then
       local meta = getmetatable(t)
       for k, v in pairs(t) do target[k] = v end
       setmetatable(target, meta)
@@ -12,7 +12,7 @@ return function (t, ...)
       if type(tab) == 'table' then
         for k, v in pairs(tab) do target[k] = v end
       else
-        error('Arguments must be of type <table>, got <' .. type(t) .. '> instead')
+        error('Arguments must be of type <table>, got <' .. type(tab) .. '> instead')
       end
     end
     return target
